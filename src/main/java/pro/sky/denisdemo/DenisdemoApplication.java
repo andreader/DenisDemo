2777ed6a3fe3ff0f05e797267b79064611a1e3b2
Андрей <79538233369@ya.ru>
2024-02-26 16:52:00 +0500

    Initial commit

---
+++ src/main/java/pro/sky/denisdemo/DenisdemoApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.denisdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DenisdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DenisdemoApplication.class, args);
+	}
+
+}
